04804c4de2b08700a1d3629141ffbec092550578
Alexe Vlad <alexevladgabriel@gmail.com>
2021-05-01 14:56:54 +0300

    First php package, don't blame my commit messages

---
+++ src/RustRcon.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Scai;
+
+use WebSocket\Client;
+
+class RustRcon
+{
+    private $client;
+    public function __construct(string $ip, int $port, string $pass)
+    {
+        $this->client = new Client("ws://{$ip}:{$port}/{$pass}");
+    }
+
+    public function sendPacket(string $command): array
+    {
+        $data = array(
+            'Identifier' => 1001,
+            'Message' => $command, // command
+            'Stacktrace' => '',
+            'Type' => 3
+        );
+        $this->client->send(json_encode($data));
+
+        return json_decode($this->client->receive());
+    }
+
+    public function __destruct()
+    {
+        $this->client = null;
+    }
+}
