7b3259415dd5d12d4323e2ea2422477c00647702
Alexe Vlad <scai@swh.local>
2023-01-28 11:36:05 +0200

    Added  and  mode, changed methods names and fixed issue with null condition.

--- src/WebRconClass.php
+++ src/WebRconClass.php
@@ -2,58 +2,90 @@
 
 namespace Scai\WebRcon;
 
-use WebSocket\BadOpcodeException;
+use Illuminate\Log;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 use WebSocket\Client;
 
 class WebRconClass
 {
+    public const LAST_INDEX = 1001;
+
     /**
      * @var Client
      */
     private Client $client;
 
-    /**
-     * @var array
-     */
-    private array $response = [];
+    private Log\Logger $logger;
 
-    public const LAST_INDEX = 1001;
+    private bool $debug = false;
+
+    public function __construct(bool $debug = false)
+    {
+        if ($debug) {
+            $this->debug = true;
+            $this->logger = new Log\Logger(new Logger('Players Module'));
+        }
+    }
 
     /**
      * @param string $ip
      * @param int $port
      * @param string $password
+     * @return void
      */
-    public function __construct(string $ip, int $port, string $password)
+    public function connect(string $ip, int $port, string $password = "CHANGEME"): void
     {
-        $this->client = new Client("ws://{$ip}:{$port}/{$password}");
+        $this->client = new Client("ws://$ip:$port/$password");
     }
 
     /**
      * @param string $command
      * @param int $identifier
-     * @param string $name
-     * @return array
-     * @throws BadOpcodeException
+     * @return void
      */
-    public function command(string $command, int $identifier = self::LAST_INDEX, string $name = "WebRcon"): array
+    public function send(string $command, int $identifier = self::LAST_INDEX): void
     {
-        $this->client->send(json_encode([
+        /**
+         * The library used require to send a text message to connect to websocket instead
+         */
+//        if (! $this->client->isConnected()) {
+//            return;
+//        }
+
+        if ($identifier == null) {
+            $identifier = -1;
+        }
+
+        $this->client->text(json_encode([
             'Identifier' => $identifier,
             'Message' => $command,
-            'Name' => $name,
+            'Name' => "WebRcon",
         ]));
 
-        return $this->response = array_map(function ($json) {
-            return json_decode($json, true);
-        }, json_decode($this->client->receive(), true));
+        if ($this->debug) {
+            $this->logger->info("running $command on ".$this->client->getRemoteName());
+        }
     }
 
     /**
-     * @return array
+     * @return ?array
      */
-    public function receive(): array
+    public function receive(): ?array
     {
-        return $this->response["Message"];
+        $response = array_map(function ($json) {
+            return json_decode($json, true);
+        }, json_decode($this->client->receive(), true));
+
+        if ($this->debug) {
+            $this->logger->info('Response', $response);
+        }
+
+        return $response;
+    }
+
+    public function isConnected(): bool
+    {
+        return $this->client->isConnected();
     }
 }
--- tests/WebRconTest.php
+++ tests/WebRconTest.php
@@ -1,12 +1,18 @@
 <?php
 
-it('can send & receive commands', function () {
-    $rcon = new Scai\WebRcon\WebRconClass('65.108.139.88', 28016, 'CHANGEME');
-    $rcon->command('serverinfo');
-    $data = $rcon->receive();
 
-    expect($data['Hostname'])
-        ->toEqual('A Rust Server');
-    expect($data['Map'])
-        ->toEqual('Procedural Map');
+$rcon = new \Scai\WebRcon\WebRconClass(true);
+$rcon->connect('192.168.1.100', 28017, 'CHANGEME');
+it('connect successfully to server', function () use ($rcon) {
+    try {
+        $rcon->send('serverinfo');
+    } catch (\WebSocket\ConnectionException $exception) {
+    }
+    expect($rcon->isConnected())->toBeTrue();
 });
+
+it('can receive commands', function () use ($rcon) {
+    $rcon->send('serverinfo');
+    $rcon->receive();
+    expect($rcon->receive())->toBeArray();
+})->depends('it connect successfully to server');
